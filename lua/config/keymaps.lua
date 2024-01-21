1efaf52a24706aa7eb3a10b53bd20a65fc9f8627
Erik Gabriel Carrillo <erik.g.carrillo@intel.com>
2024-01-21 14:57:31 -0600

    Add FZF.vim plugin

--- lua/config/keymaps.lua
+++ lua/config/keymaps.lua
@@ -5,6 +5,8 @@ vim.keymap.set("n", "<leader>e", ":NvimTreeFocus<CR>", { noremap = true })
 vim.keymap.set("n", "<leader>n", ":NvimTreeToggle<CR>", { noremap = true })
 vim.keymap.set("n", "<leader>sB", "<cmd>Telescope builtin<cr>")
 -- nvim-window
+-- FZF
+vim.keymap.set("n", "<leader>t", "<cmd>Tags<cr>", { desc = "Fzf tags search" })
 vim.keymap.set("n", "_", "<cmd>:lua require('nvim-window').pick()<CR>", { desc = "Pick a window" })
 
 -- delete default lazyvim mappings to switch buffers
