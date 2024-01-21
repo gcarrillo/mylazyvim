b035ad6694761ebf4c373b0940479cb3ad19cab8
Erik Gabriel Carrillo <erik.g.carrillo@intel.com>
2024-01-21 15:16:57 -0600

    Some more options settings

--- lua/config/options.lua
+++ lua/config/options.lua
@@ -7,8 +7,17 @@ opt.colorcolumn = "80"
 opt.list = false
 opt.shiftwidth = 8
 opt.tabstop = 8
+opt.cursorline = false
+opt.scrolloff = 0
+-- https://neovim.io/doc/user/options.html#'cindent'
+-- If the indentexpr option is not empty, it overrides cindent, and lazyvim sets
+-- it via the treesitter config. Can either unset it as below, or turn off
+-- "indent" in the treesitter configuration.
+-- opt.indentexpr = ""
+opt.cindent = true
+opt.cino = "(0"
 
--- This will cause the status line to display in every window. Only need to 
+-- This will cause the status line to display in every window. Only need to
 -- set this if we want to use ChooseWin with Lualine disabled
 -- opt.laststatus = 2
 opt.mouse = ""
