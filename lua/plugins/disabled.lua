fd03cf202c58f1f31f7e63ece2a126e7ca8c546b
Erik Gabriel Carrillo <erik.g.carrillo@intel.com>
2024-05-13 08:38:18 -0500

    disable tree-sitter-context
    Signed-off-by: Erik Gabriel Carrillo <erik.g.carrillo@intel.com>

--- lua/plugins/disabled.lua
+++ lua/plugins/disabled.lua
@@ -3,4 +3,6 @@ return {
   { "lukas-reineke/indent-blankline.nvim", enabled = false },
   -- disable mini.indentscope
   { "echasnovski/mini.indentscope", enabled = false },
+  -- disable treesitter-context
+  { "nvim-treesitter/nvim-treesitter-context", enabled = false },
 }
