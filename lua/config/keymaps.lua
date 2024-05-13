8485392ec94997bb8faf587ae2a42a17738cd9cc
Erik Gabriel Carrillo <erik.g.carrillo@intel.com>
2024-05-13 09:06:09 -0500

    Add keymap for nvim-window plugin
    Signed-off-by: Erik Gabriel Carrillo <erik.g.carrillo@intel.com>

--- lua/config/keymaps.lua
+++ lua/config/keymaps.lua
@@ -1,3 +1,6 @@
 -- Keymaps are automatically loaded on the VeryLazy event
 -- Default keymaps that are always set: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/keymaps.lua
 -- Add any additional keymaps here
+
+-- nvim-window
+vim.keymap.set("n", "_", "<cmd>:lua require('nvim-window').pick()<CR>", { desc = "Pick a window" })
