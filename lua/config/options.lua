3181f0f963a4e4552b3ad50343fd22a844794a60
Erik Gabriel Carrillo <erik.g.carrillo@intel.com>
2024-05-13 08:50:01 -0500

    Add my personal options in
    Signed-off-by: Erik Gabriel Carrillo <erik.g.carrillo@intel.com>

--- lua/config/options.lua
+++ lua/config/options.lua
@@ -1,3 +1,20 @@
 -- Options are automatically loaded before lazy.nvim startup
 -- Default options that are always set: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/options.lua
 -- Add any additional options here
+local opt = vim.opt
+
+opt.colorcolumn = "80"
+opt.list = false
+opt.shiftwidth = 8
+opt.tabstop = 8
+opt.cursorline = false
+opt.scrolloff = 0
+-- https://neovim.io/doc/user/options.html#'cindent'
+-- If the indentexpr option is not empty, it overrides cindent, and lazyvim sets
+-- it via the treesitter config. Can either unset it as below, or turn off
+-- "indent" in the treesitter configuration.
+-- opt.indentexpr = ""
+opt.cindent = true
+opt.cino = "(0"
+
+opt.mouse = ""
